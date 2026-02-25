80d3350af8c3fd383ab7d36aca25433d70f0bbbc
marstusch <git@marstusch.de>
2026-02-25 13:54:19 +0100

    RuntimeConfiguration ergänzt

--- deployment/src/main/java/de/mtgz/logging/extension/deployment/LoggingExtensionProcessor.java
+++ deployment/src/main/java/de/mtgz/logging/extension/deployment/LoggingExtensionProcessor.java
@@ -1,9 +1,17 @@
 package de.mtgz.logging.extension.deployment;
 
+import de.mtgz.logging.LoggingRequestFilter;
+import de.mtgz.logging.LoggingResponseFilter;
+import de.mtgz.logging.correlation.CorrelationIdClientRequestFilter;
+import de.mtgz.logging.correlation.CorrelationIdRequestFilter;
+import de.mtgz.logging.correlation.CorrelationIdResponseFilter;
 import de.mtgz.logging.injection.LoggerProducer;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.RunTimeConfigurationDefaultBuildItem;
+
+import java.util.List;
 
 class LoggingExtensionProcessor {
 
@@ -18,7 +26,27 @@ class LoggingExtensionProcessor {
     AdditionalBeanBuildItem registerBeans() {
         return AdditionalBeanBuildItem.builder()
                 .addBeanClass(LoggerProducer.class)
+                .addBeanClass(LoggingRequestFilter.class)
+                .addBeanClass(LoggingResponseFilter.class)
+                .addBeanClass(CorrelationIdClientRequestFilter.class)
+                .addBeanClass(CorrelationIdRequestFilter.class)
+                .addBeanClass(CorrelationIdResponseFilter.class)
                 .setUnremovable()
                 .build();
+    }
+
+    @BuildStep
+    List<RunTimeConfigurationDefaultBuildItem> defaults() {
+        return List.of(
+                // Console JSON
+                new RunTimeConfigurationDefaultBuildItem("quarkus.log.console.json", "true"),
+                new RunTimeConfigurationDefaultBuildItem("quarkus.log.console.json.enabled", "true"),
+                new RunTimeConfigurationDefaultBuildItem("quarkus.log.console.json.pretty-print", "true"),
+
+                // OTel defaults (funktioniert nur, wenn quarkus-opentelemetry auch als dependency da ist)
+                new RunTimeConfigurationDefaultBuildItem("quarkus.otel.enabled", "true"),
+                new RunTimeConfigurationDefaultBuildItem("quarkus.otel.traces.enabled", "true"),
+                new RunTimeConfigurationDefaultBuildItem("quarkus.otel.metrics.enabled", "true")
+        );
     }
 }
